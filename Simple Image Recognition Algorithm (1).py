b24345300b6036611da0c96c85839785eee9b803
Mostafa Ahmed Zaki <46458672+MostafaAhmedZaki@users.noreply.github.com>
2021-05-14 19:40:23 +0200

    Update Simple Image Recognition Algorithm (1).py

--- Simple Image Recognition Algorithm (1).py
+++ Simple Image Recognition Algorithm (1).py
@@ -3,17 +3,8 @@
 
 # # Simple Image Recognition Algorithm
 
-# ## Table of Contents
-# - [1 - Packages](#1)
-# - [2 - Overview of the Problem set](#2)
-# - [3 - Building the parts of our algorithm](#3)
-# - [4 - Merge all functions into a model](#4)
 
-# <a name='1'></a>
-# ## 1 - Packages ##
-# 
-
-# In[39]:
+# In[1]:
 
 
 import numpy as np
@@ -26,18 +17,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# <a name='2'></a>
-# ## 2 - Overview of the Problem set ##
-# 
-# **Problem Statement**: Given a dataset ("data.h5") containing:
-#     - a training set of m_train images labeled as cat (y=1) or non-cat (y=0)
-#     - a test set of m_test images labeled as cat or non-cat
-#     - each image is of shape (num_px, num_px, 3) where 3 is for the 3 channels (RGB). Thus, each image is square (height = num_px) and (width = num_px).
-# 
-# I will build a simple image-recognition algorithm that can correctly classify pictures as cat or non-cat.
-
-# In[86]:
+# In[2]:
 
 
 def load_dataset():
@@ -56,13 +36,13 @@ def load_dataset():
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 
-# In[41]:
+# In[3]:
 
 
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
 
-# In[42]:
+# In[4]:
 
 
 m_train = train_set_x_orig.shape[0]
@@ -78,7 +58,7 @@ print ("test_set_x shape: " + str(test_set_x_orig.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 
 
-# In[43]:
+# In[5]:
 
 
 train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
@@ -91,17 +71,13 @@ print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 
 
-# In[44]:
+# In[6]:
 
 
 train_set_x = train_set_x_flatten/255.
 test_set_x = test_set_x_flatten/255.
 
-
-# <a name='3'></a>
-# ## 3 - Building the parts of our algorithm ## 
-
-# In[45]:
+# In[7]:
 
 
 def sigmoid(z):
@@ -110,13 +86,13 @@ def sigmoid(z):
     return s
 
 
-# In[46]:
+# In[8]:
 
 
 print ("sigmoid([0, 2]) = " + str(sigmoid(np.array([0,2]))))
 
 
-# In[47]:
+# In[9]:
 
 
 x = np.array([0.5, 0, 2.0])
@@ -124,7 +100,7 @@ output = sigmoid(x)
 print(output)
 
 
-# In[48]:
+# In[10]:
 
 
 def initialize_with_zeros(dim):
@@ -136,7 +112,7 @@ def initialize_with_zeros(dim):
     return w, b
 
 
-# In[49]:
+# In[11]:
 
 
 dim = 2
@@ -145,7 +121,7 @@ print ("w = " + str(w))
 print ("b = " + str(b))
 
 
-# In[50]:
+# In[12]:
 
 
 def propagate(w, b, X, Y):
@@ -162,7 +138,7 @@ def propagate(w, b, X, Y):
     return grads, cost
 
 
-# In[51]:
+# In[12]:
 
 
 w, b, X, Y = np.array([[1.],[2.]]), 2., np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
@@ -172,7 +148,7 @@ print ("db = " + str(grads["db"]))
 print ("cost = " + str(cost))
 
 
-# In[52]:
+# In[13]:
 
 
 def optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=False):
@@ -200,7 +176,7 @@ def optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=Fal
     return params, grads, costs
 
 
-# In[53]:
+# In[14]:
 
 
 params, grads, costs = optimize(w, b, X, Y, num_iterations= 100, learning_rate = 0.009, print_cost = False)
@@ -212,7 +188,7 @@ print ("db = " + str(grads["db"]))
 print("Costs = " + str(costs))
 
 
-# In[54]:
+# In[15]:
 
 
 def predict(w, b, X):
@@ -227,7 +203,7 @@ def predict(w, b, X):
     return Y_prediction
 
 
-# In[55]:
+# In[16]:
 
 
 w = np.array([[0.1124579],[0.23106775]])
@@ -235,11 +211,7 @@ b = -0.3
 X = np.array([[1.,-1.1,-3.2],[1.2,2.,0.1]])
 print ("predictions = " + str(predict(w, b, X)))
 
-
-# <a name='4'></a>
-# ## 4 - Merge all functions into a model ##
-
-# In[56]:
+# In[17]:
 
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
@@ -267,13 +239,13 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
     return d
 
 
-# In[78]:
+# In[18]:
 
 
 logistic_regression_model = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
 
 
-# In[59]:
+# In[19]:
 
 
 costs = np.squeeze(logistic_regression_model['costs'])
@@ -284,7 +256,7 @@ plt.title("Learning rate =" + str(logistic_regression_model["learning_rate"]))
 plt.show()
 
 
-# In[60]:
+# In[20]:
 
 
 import IPython.display as display
@@ -293,7 +265,7 @@ image_path = 'my_image.jpg'
 display.display(Image.open(image_path))
 
 
-# In[71]:
+# In[21]:
 
 
 my_image = "my_image.jpg"   
@@ -305,7 +277,7 @@ my_predicted_image = predict(logistic_regression_model["w"], logistic_regression
 print("y = " + str(np.squeeze(my_predicted_image)) + ", algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
 
 
-# In[62]:
+# In[22]:
 
 
 import IPython.display as display
@@ -314,7 +286,7 @@ image_path = 'my_image2.jpg'
 display.display(Image.open(image_path))
 
 
-# In[70]:
+# In[23]:
 
 
 my_image = "my_image2.jpg"   
@@ -326,7 +298,7 @@ my_predicted_image = predict(logistic_regression_model["w"], logistic_regression
 print("y = " + str(np.squeeze(my_predicted_image)) + ", algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
 
 
-# In[84]:
+# In[24]:
 
 
 import IPython.display as display
@@ -335,7 +307,7 @@ image_path = 'download (1).jpg'
 display.display(Image.open(image_path))
 
 
-# In[85]:
+# In[25]:
 
 
 my_image = "download (1).jpg"   
